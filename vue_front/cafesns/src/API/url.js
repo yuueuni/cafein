125d9a3b18fd1f4122dd438407777d3bbfd5cc9a
김가윤 <gayun1109@naver.com>
2020-07-24 09:03:20 +0900

    like,stamp,follow

---
+++ vue_front/cafesns/src/API/url.js
@@ -0,0 +1,24 @@
+export default{
+    URL: 'http://localhost:9999',
+    ROUTES: {
+        //accounts 
+        signup: '/api/user/signup/',
+        login: '/api/user/login/',
+        logout: '',
+        mypage: '/api/user/',
+
+        //post
+        createPost: '',
+
+        //like, stamp
+        like: '/like/',
+        stamp: '',
+
+        //follow
+        following: '',
+        folower: '',
+
+
+
+    }
+}
