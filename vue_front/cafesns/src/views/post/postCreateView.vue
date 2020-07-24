3ed22014bd7ba2a86812f8bcc8acad348ee05997
yoon <yuueuni@gmail.com>
2020-07-24 09:59:39 +0900

    Modify Post Create

--- vue_front/cafesns/src/views/post/postCreateView.vue
+++ vue_front/cafesns/src/views/post/postCreateView.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
+    <router-link to="/post/detail"><v-btn text>go post</v-btn></router-link>
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8" lg="6">
-        <v-card ref="form" class="px-3 d-inline-block">
+        <v-card ref="form" class="d-inline-block px-3">
           <v-card-text class="text-center">
             <v-card-title>
               <h1>New Post</h1>
@@ -10,76 +11,74 @@
               <span class="text-subtitle-2">cafe name</span>
             </v-card-title>
             <v-divider></v-divider>
-            <v-row>
-              <v-col
-                cols="12"
-              >
-                <!-- taste -->
-                <v-btn-toggle
-                  tile
-                  color="deep-purple accent-3"
-                  group
-                  mandatory
-                >
-                  <v-btn depressed @click="postList.taste = true">
-                    <v-icon left>mdi-thumb-up</v-icon>
-                    <span>like</span>
-                  </v-btn>
-                  <v-btn>
-                    맛
-                  </v-btn>
-                  <v-btn depressed @click="postList.taste = false">
-                    <v-icon left>mdi-thumb-down</v-icon>
-                    <span>unlike</span>
-                  </v-btn>
-                </v-btn-toggle>
 
-                <!-- mood -->
-                <v-btn-toggle
-                  tile
-                  color="deep-purple accent-3"
-                  group
-                  mandatory
-                >
-                  <v-btn depressed @click="postList.mood = true">
-                    <v-icon left>mdi-thumb-up</v-icon>
-                    <span>like</span>
-                  </v-btn>
-                  <v-btn>
-                    분위기
-                  </v-btn>
-                  <v-btn depressed @click="postList.mood = false">
-                    <v-icon left>mdi-thumb-down</v-icon>
-                    <span>unlike</span>
-                  </v-btn>
-                </v-btn-toggle>
+            <!-- taste -->
+            <v-btn-toggle
+              tile
+              color="deep-purple accent-3"
+              group
+              mandatory
+              class="post-checklist"
+            >
+              <v-btn depressed @click="postList.taste = true">
+                <v-icon left>mdi-thumb-up</v-icon>
+                <span>like</span>
+              </v-btn>
+              <v-btn depressed class="disabled-button">
+                <span>맛</span>
+              </v-btn>
+              <v-btn depressed @click="postList.taste = false">
+                <v-icon left>mdi-thumb-down</v-icon>
+                <span>unlike</span>
+              </v-btn>
+            </v-btn-toggle>
 
-                <!-- clean -->
-                <v-btn-toggle
-                  tile
-                  color="deep-purple accent-3"
-                  group
-                  mandatory
-                >
-                  <v-btn depressed @click="postList.clean = true">
-                    <v-icon left>mdi-thumb-up</v-icon>
-                    <span>like</span>
-                  </v-btn>
-                  <v-btn>
-                    청결도
-                  </v-btn>
-                  <v-btn depressed @click="postList.clean = false">
-                    <v-icon left>mdi-thumb-down</v-icon>
-                    <span>unlike</span>
-                  </v-btn>
-                </v-btn-toggle>
-              </v-col>
-            </v-row>
+            <!-- mood -->
+            <v-btn-toggle
+              tile
+              color="deep-purple accent-3"
+              group
+              mandatory
+              class="post-checklist"
+            >
+              <v-btn depressed @click="postList.mood = true">
+                <v-icon left>mdi-thumb-up</v-icon>
+                <span>like</span>
+              </v-btn>
+              <v-btn depressed class="disabled-button">
+                <span>분위기</span>
+              </v-btn>
+              <v-btn depressed @click="postList.mood = false">
+                <v-icon left>mdi-thumb-down</v-icon>
+                <span>unlike</span>
+              </v-btn>
+            </v-btn-toggle>
+
+            <!-- clean -->
+            <v-btn-toggle
+              tile
+              color="deep-purple accent-3"
+              group
+              mandatory
+              class="post-checklist"
+            >
+              <v-btn depressed @click="postList.clean = true">
+                <v-icon left>mdi-thumb-up</v-icon>
+                <span>like</span>
+              </v-btn>
+              <v-btn depressed class="disabled-button">
+                <span>위생</span>
+              </v-btn>
+              <v-btn depressed @click="postList.clean = false">
+                <v-icon left>mdi-thumb-down</v-icon>
+                <span>unlike</span>
+              </v-btn>
+            </v-btn-toggle>
 
             <v-textarea 
               label="Content" 
-              v-model="postList.contents" 
-              id="content" 
+              v-model="postList.contents"
+              id="content"
             >
             </v-textarea>
 
@@ -124,6 +123,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.post-checklist {
+  justify-content: center;
+  width: 100%;
+  margin-top: 1rem;
+}
+
+.disabled-button {
+  pointer-events: none;
+}
 
 </style>
