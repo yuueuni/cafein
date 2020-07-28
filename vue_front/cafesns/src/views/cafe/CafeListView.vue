e6aaffe6db785a03c5651ab9b06849131e3666a0
박유은 <yuueuni@gmail.com>
2020-07-28 21:52:46 +0900

    #S03P12A203-48 cafe list, detail view

---
+++ vue_front/cafesns/src/views/cafe/CafeListView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-card
+      class="ma-4"
+      v-for="cafe in cafeList"
+      :key="cafe.id"
+    >
+      <div class="subheading">
+        <v-btn @click="onSelectCafe(cafe.cafeno)" text>{{ cafe.name }}</v-btn>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'CafeList',
+  computed: {
+    ...mapState(['cafeList'])
+  },
+  methods: {
+    ...mapActions(['fetchCafeList']),
+    onSelectCafe(target) {
+      this.$router.push(`/cafe/detail/${target}`)
+    }
+  },
+  created() {
+    this.fetchCafeList()
+  },
+}
+</script>
+
+<style>
+
+</style>
