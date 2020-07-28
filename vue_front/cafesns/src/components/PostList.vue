dfb5cc02a36a07a6e4cdfee537dbd0a8ddd6d5d2
박유은 <yuueuni@gmail.com>
2020-07-28 21:54:03 +0900

    #S03P12A203-46 post list view

---
+++ vue_front/cafesns/src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>Cafe keywords</v-list-item-title>
+      <v-list-item-subtitle>
+        <v-chip-group
+          active-class="deep-purple--text text--accent-4"
+          mandatory
+        >
+          <v-chip disabled>키워드1</v-chip>
+          <v-chip disabled>키워드2</v-chip>
+          <v-chip disabled>키워드3</v-chip>
+        </v-chip-group>
+      </v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+
+}
+</script>
+
+<style>
+
+</style>
