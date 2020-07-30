e4f8924a0ef4847b46e21c76ca748c1c7fd93868
박유은 <yuueuni@gmail.com>
2020-07-30 21:38:17 +0900

    #S03P12A203-55 Add  post detail

--- vue_front/cafesns/src/views/post/PostDetailView.vue
+++ vue_front/cafesns/src/views/post/PostDetailView.vue
@@ -1,97 +1,63 @@
 <template>
   <div>
-    <v-btn @click="$router.go(-1)">뒤로</v-btn>
     <v-row justify="center">
-      <v-col cols="12" sm="10" md="8" lg="6">
-        <v-card ref="form" class="d-inline-block px-3 pb-3">
+      <v-col
+        cols="12"
+        sm="10"
+        md="8"
+        lg="6"
+      >
+        <v-card ref="form" class="px-3">
           <v-card-text class="text-center">
             <v-card-title>
-              <h1>Cafe name</h1>
+              <h1>{{ selectedPost.uid }}</h1>
               <v-spacer></v-spacer>
-              <span class="text-subtitle-2">user</span>
+              <span class="text-subtitle-2">{{ selectedPost.cafeno }}</span>
             </v-card-title>
-            <v-divider></v-divider>
+            <v-divider class="mb-3"></v-divider>
 
-            <!-- taste -->
-            <v-btn-toggle
-              tile
-              color="deep-purple accent-3"
-              group
-              mandatory
-              class="post-checklist"
-            >
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-up</v-icon>
-                <span>like</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <span>맛</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-down</v-icon>
-                <span>unlike</span>
-              </v-btn>
-            </v-btn-toggle>
-
-            <!-- mood -->
-            <v-btn-toggle
-              tile
-              color="deep-purple accent-3"
-              group
-              mandatory
-              class="post-checklist"
-            >
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-up</v-icon>
-                <span>like</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <span>분위기</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-down</v-icon>
-                <span>unlike</span>
-              </v-btn>
-            </v-btn-toggle>
-
-            <!-- clean -->
-            <v-btn-toggle
-              tile
-              color="deep-purple accent-3"
-              group
-              mandatory
-              class="post-checklist"
-            >
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-up</v-icon>
-                <span>like</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <span>위생</span>
-              </v-btn>
-              <v-btn depressed class="disabled-button">
-                <v-icon left>mdi-thumb-down</v-icon>
-                <span>unlike</span>
-              </v-btn>
-            </v-btn-toggle>
-
-            <v-textarea
-              label="Content" 
+            <span>맛 {{ selectedPost.taste }}</span>
+            <v-rating
+              v-model="selectedPost.taste"
+              color="yellow darken-3"
+              background-color="grey darken-1"
+              empty-icon="$ratingFull"
+              half-increments
+              hover
               disabled
-              placeholder="(유저 작성 글) Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at."
-            >
-            </v-textarea>
+            ></v-rating>
 
-            <div style="height:200px; border:1px solid black">
-              사진
-            </div>
-            <v-img contain max-width="100%" max-height="300px"></v-img>
+            <span>분위기 {{ selectedPost.mood }}</span>
+            <v-rating
+              v-model="selectedPost.mood"
+              color="yellow darken-3"
+              background-color="grey darken-1"
+              empty-icon="$ratingFull"
+              half-increments
+              hover
+              disabled
+            ></v-rating>
+
+            <span>위생 {{ selectedPost.clean }}</span>
+            <v-rating
+              v-model="selectedPost.clean"
+              color="yellow darken-3"
+              background-color="grey darken-1"
+              empty-icon="$ratingFull"
+              half-increments
+              hover
+              disabled
+            ></v-rating>
+          <v-divider></v-divider>
+            <p class="my-3 text-start" style="min-height:100px;">{{ selectedPost.contents }}</p>
+
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" contain max-width="100%" max-height="300px"></v-img>
           </v-card-text>
-          <v-card-actions>
+          <!-- <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn text color="secondary">삭제</v-btn>
             <v-btn text color="primary">수정</v-btn>
-          </v-card-actions>
+          </v-card-actions> -->
         </v-card>
       </v-col>
     </v-row>
@@ -101,6 +67,8 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 import CommentCreate from '@/components/CommentCreate.vue'
 import CommentList from '@/components/CommentList.vue'
 
@@ -109,6 +77,20 @@ export default {
   components: {
     CommentCreate,
     CommentList,
+  },
+  data() {
+    return {
+      postId: this.$route.params.post_id,
+    }
+  },
+  computed: {
+    ...mapState(['selectedPost'])
+  },
+  methods: {
+    ...mapActions(['postDetail']),
+  },
+  created() {
+    this.postDetail(this.postId)
   }
 }
 </script>
