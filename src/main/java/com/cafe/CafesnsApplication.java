5158248239bb57c88b159d5bcfecf382ddddbb24
Jiho <wlgh325@naver.com>
2020-08-20 00:31:24 +0900

    filter 적용

--- src/main/java/com/cafe/CafesnsApplication.java
+++ src/main/java/com/cafe/CafesnsApplication.java
@@ -2,13 +2,17 @@ package com.cafe;
 import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class CafesnsApplication {
+@ServletComponentScan
+public class CafesnsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafesnsApplication.class, args);
@@ -27,5 +31,9 @@ public class CafesnsApplication {
 		connector.setPort(8070);
         return connector;
 	}
-	
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(SpringBootApplication.class);
+    }
 }
